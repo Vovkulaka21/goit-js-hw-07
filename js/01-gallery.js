c17b4be6739c79514313f630cc531a1bccde386f
Vovkulaka21 <110464093+Vovkulaka21@users.noreply.github.com>
2023-01-21 06:21:59 +0200

    1 task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,43 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainerEl = document.querySelector(".gallery");
+const imagesMarkup = createItemsMarkup(galleryItems);
+galleryContainerEl.insertAdjacentHTML("beforeend", imagesMarkup);
+
+
+
+
+
+
+
+function createItemsMarkup() {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+      <a class="gallery__link" href="${original.value}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+      </a>
+    </div>`;
+    })
+    .join("");
+}
+
+const onContainerClick = (event) => {
+  event.preventDefault();
+
+  if (event.target.classList.contains("gallery")) return;
+  const source = event.target.dataset.source;
+
+  const instance = basicLightbox.create(`
+    <img src="${source}"width="800" height="600">`);
+
+  instance.show();
+};
+
+galleryContainerEl.addEventListener("click", onContainerClick);
